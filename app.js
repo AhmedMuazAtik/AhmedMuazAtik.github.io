e100e5b92da804ccd52ee1bc4bf61cb2205d235c
Ahmed Muaz Atik <95479218+AhmedMuazAtik@users.noreply.github.com>
2023-12-29 01:55:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
   renderTasks();
   renderHistory();
 
+  // Function to clear all tasks
+  window.clearAll = function () {
+    // Clear the tasks array
+    tasks = [];
+    // Render the updated tasks
+    renderTasks();
+  };
+
   // Function to clear the history
   window.clearHistory = function () {
     // Clear the history array
@@ -59,30 +67,107 @@ document.addEventListener("DOMContentLoaded", function () {
       const cell2 = row.insertCell(1);
 
       cell1.textContent = `${task.text} - ${task.status || 'No status'}`;
-      cell2.textContent = task.timeout ? "Time Out" : "";
+      cell2.textContent = task.timeout ? "" : "";
     });
 
     // Save history to local storage
     saveHistory();
   }
 
-  // Function to add a new task
-  window.addTask = function () {
-    const taskText = taskInput.value.trim();
-    const dueDate = taskDate.value;
+ // Function to add a new task
+window.addTask = function () {
+  const taskText = taskInput.value.trim();
+  const dueDate = taskDate.value;
 
-    if (taskText !== "") {
-      const newTask = {
-        text: taskText,
-        completed: false,
-        dueDate: dueDate,
-      };
+  // Check if task text is empty
+  if (taskText === "") {
+    alert("Please enter a task description.");
+    return;
+  }
+
+  // Check if due date is set
+  if (dueDate) {
+    const newTask = {
+      text: taskText,
+      completed: false,
+      dueDate: dueDate,
+    };
+
+    // Convert due date to milliseconds
+    const dueTime = new Date(dueDate).getTime();
+
+    // Calculate time remaining
+    const currentTime = new Date().getTime();
+    const timeRemaining = dueTime - currentTime;
+
+    // Schedule an alert when the time is up
+    setTimeout(() => {
+      alert(`Time is up for task: ${newTask.text}`);
+      markAsTimedOut(newTask);
+    }, timeRemaining);
+
+    // Schedule an alert 1 minute before the due time
+    const oneMinuteBefore = timeRemaining - 60000; // 60,000 milliseconds = 1 minute
+    if (oneMinuteBefore > 0) {
+      setTimeout(() => {
+        alert(`One minute left for task: ${newTask.text}`);
+      }, oneMinuteBefore);
+    }
+
+    tasks.push(newTask);
+    renderTasks();
+    taskInput.value = "";
+    taskDate.value = "";
+  } else {
+    // Alert the user that a due date is required
+    alert("Please enter a due date for the task.");
+  }
+};
+
+// Function to show a notification
+function showNotification(message, task) {
+  // Check if the Notification API is supported
+  if ("Notification" in window) {
+    // Request permission to show notifications
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        // Display the notification
+        new Notification("Task Notification", { body: message });
+      }
+    });
+  }
+}
+
+// Function to mark a task as timed out
+function markAsTimedOut(task) {
+  // Move the task to history with the status "Timed Out"
+  task.timeout = true;
+  task.status = "Timed Out";
+  history.push(task);
+
+  // Apply animation for scoring out the task
+  const taskListItems = document.querySelectorAll('#taskList li');
+  const taskIndex = tasks.findIndex((t) => t === task);
+  if (taskIndex !== -1) {
+    const taskListItem = taskListItems[taskIndex];
+    taskListItem.classList.add('timedOut', 'scoreOutAnimation');
+  }
+
+  // Remove the task from the current tasks after animation completes
+  setTimeout(() => {
+    const taskIndex = tasks.findIndex((t) => t === task);
+    if (taskIndex !== -1) {
+      tasks.splice(taskIndex, 1);
+      renderTasks();
+      renderHistory();
+    }
+  }, 500); // Adjust the time based on your animation duration
+}
+
       tasks.push(newTask);
       renderTasks();
       taskInput.value = "";
       taskDate.value = "";
-    }
-  };
 
   // Function to delete a task
   function deleteTask(index) {
